230b7c0ea9110b67a539e506dae05af75a55c7b7
DeanMichiels <PS210022@edu.summacollege.nl>
2024-10-27 14:40:28 +0100

    Added a brands table that has a relation to the sneakers table

--- SneakerSZN/SneakerSZN_BLL/Models/Brand.cs
+++ SneakerSZN/SneakerSZN_BLL/Models/Brand.cs
@@ -6,15 +6,10 @@ using System.Threading.Tasks;
 
 namespace SneakerSZN_BLL.Models
 {
-    public class Sneaker
+    public class Brand
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Size { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-
-        public int BrandId { get; set; }
-        public Brand Brand { get; set; }
+        public ICollection<Sneaker> Sneakers { get; set; }
     }
 }
