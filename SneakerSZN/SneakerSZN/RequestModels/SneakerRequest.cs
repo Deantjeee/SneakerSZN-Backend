408233bde2273407ae312c9ce049a3a7b35e4459
DeanMichiels <PS210022@edu.summacollege.nl>
2024-10-31 21:16:27 +0100

    images working

--- SneakerSZN/SneakerSZN/RequestModels/SneakerRequest.cs
+++ SneakerSZN/SneakerSZN/RequestModels/SneakerRequest.cs
@@ -8,6 +8,7 @@ namespace SneakerSZN.RequestModels
         public int Size { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
+
         public int BrandId { get; set; }
     }
 }
